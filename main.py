ef2e720e1d72fee958c6875f5a35541849f45955
aryab2003 <bhattacharyyaarya165@gmail.com>
2024-05-07 21:26:34 +0530

    feat:added corrected words

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from model import spell_check
+import enchant
 
 app = Flask(__name__)
 
@@ -13,8 +14,14 @@ def index():
 def check_spelling():
     text = request.form["text"]
     misspelled_words = spell_check(text)
+    corrected_words = []
+    for word in misspelled_words:
+        suggestions = enchant.Dict("en_US").suggest(word)
+        corrected_word = suggestions[0] if suggestions else "No suggestion found"
+        corrected_words.append(corrected_word)
     result = (
-        f"Misspelled words: {', '.join(misspelled_words)}"
+        f"Misspelled words: {', '.join(misspelled_words)} and "
+        f"Corrected words: {', '.join(corrected_words)}"
         if misspelled_words
         else "No misspelled words found"
     )
